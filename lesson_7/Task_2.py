5d558b9cedb873991ed34b786fa4f26aa11a50e9
nklevin <klevin.n@yandex.ru>
2022-01-21 10:31:07 +0300

    Lesson 7

---
+++ lesson_7/Task_2.py
@@ -0,0 +1,42 @@
+class Textil:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_j(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Total fabric area: \n'
+                   f' {(self.width / 6.5 + 0.5) + (self.height * 2 + 0.3)}')
+
+
+class Coat(Textil):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_c = round(self.width / 6.5 + 0.5)
+
+    def __str__(self):
+        return f'Area per coat: {self.square_c}'
+
+
+class Suit(Textil):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_j = round(self.height * 2 + 0.3)
+
+    def __str__(self):
+        return f'Square per suit: {self.square_j}'
+
+coat = Coat(2, 4)
+suit = Suit(1, 2)
+print(coat)
+print(suit)
+print(coat.get_sq_full)
+print(suit.get_sq_full)
+print(suit.get_square_c())
+print(suit.get_square_j())
